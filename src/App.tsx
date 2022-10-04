3859e3b61944ff070e65cc48a600dd96a3086aa2
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-10-04 12:14:37 +0800

    Add PathDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './css/App.css';
+import ChatBar from './components/ChatBar';
+import ChatWindow from './components/ChatWindow';
+import PathDisplay from './components/PathDisplay';
 
 function App() {
-    return <div></div>;
+    const [refPath, setRefPath] = useState(
+        'Users/Y0wdXpIeSTR9mBN6b5v22WUkF4o2/ConsultHistory/'
+    );
+    return (
+        <div className='appContainer'>
+            <PathDisplay path={refPath} setPath={setRefPath} />
+            <ChatWindow />
+            <ChatBar />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ChatBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ChatBar() {
+    return (
+        <div className='chatBarContainer'>
+            <input type='text' placeholder='' />
+            <button>Send</button>
+        </div>
+    );
+}
