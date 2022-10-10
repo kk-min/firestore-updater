a17d6e705096740b79a5f2e787867bde7447c9d0
MINK <76023265+kk-min@users.noreply.github.com>
2022-10-10 11:39:44 +0800

    Implement authenticateHandler, add loading and error states to Authenticator.tsx

--- src/components/Authenticator.tsx
+++ src/components/Authenticator.tsx
@@ -1,32 +1,67 @@
 import React, { useState } from 'react';
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import '../css/App.css';
 
 interface PropTypes {}
 
 export default function Authenticator(props: PropTypes) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
 
 	const authenticateHandler = () => {
 		console.log('Authenticating...');
+		setLoading(true);
+		setTimeout(() => {
+			signInWithEmailAndPassword(auth, username, password)
+				.then((userCredential) => {
+					console.log('Logged In Successfully');
+					const user = userCredential.user;
+					setLoading((prev) => false);
+				})
+				.catch((error) => {
+					console.log('Wrong Email/Password.');
+					setLoading((prev) => false);
+				});
+		}, 2000);
 	};
 
 	return (
-		<div>
-			<input
-				type='text'
-				placeholder='Email/Username'
-				onChange={(event) => {
-					setUsername((prev) => event.target.value);
-				}}
-			/>
-			<input
-				type='password'
-				placeholder='Password'
-				onChange={(event) => {
-					setPassword((prev) => event.target.value);
-				}}
-			/>
-			<button onClick={authenticateHandler}>Authenticate</button>
+		<div className='authenticator'>
+			{loading ? (
+				<div className='lds-ring' />
+			) : error ? (
+				<>
+					<h1>Authentication failed!</h1>
+					<button
+						onClick={() => {
+							setError((prev) => false);
+						}}
+					>
+						Return
+					</button>
+				</>
+			) : (
+				<>
+					<input
+						type='text'
+						placeholder='Email/Username'
+						onChange={(event) => {
+							setUsername((prev) => event.target.value);
+						}}
+					/>
+					<input
+						type='password'
+						placeholder='Password'
+						onChange={(event) => {
+							setPassword((prev) => event.target.value);
+						}}
+					/>
+					<button onClick={authenticateHandler}>Authenticate</button>
+				</>
+			)}
 		</div>
 	);
 }
